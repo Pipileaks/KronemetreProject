a6e1ef09f6a4df2671edabc7a4ccf4ae3dc2dd78
Umut Sever <severumut@yahoo.com>
2019-04-13 00:30:28 +0300

    Initial Commit

---
+++ NewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Umut Sever on 13.04.2019.
+//  Copyright © 2019 Umut Sever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
