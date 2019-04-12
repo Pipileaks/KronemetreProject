e9c5b410e2a97a8d7b1c7db67d0bb67b5679eda4
Umut Sever <severumut@yahoo.com>
2019-04-13 01:31:12 +0300

    3.sayfa Hazır

---
+++ NewProject/FWViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FWViewController.swift
+//  NewProject
+//
+//  Created by Umut Sever on 13.04.2019.
+//  Copyright © 2019 Umut Sever. All rights reserved.
+//
+
+import UIKit
+
+class FWViewController: UIViewController {
+    
+    @IBOutlet weak var sonText: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
